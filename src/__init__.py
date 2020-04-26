28e6ee2d565c9bc20b0ed0f7da0792c67d7801f2
deb <deb@fed.fritz.box>
2020-04-26 15:56:13 +0200

    for overview and deckbrowser

--- src/__init__.py
+++ src/__init__.py
@@ -1,7 +1,8 @@
 # AGPLv3
 # Copyright (c) 2013 Steve AW
 #           (c) 2016-2017 Glutanimate
-#           (c) 2019 ijgnd
+#           (c) 2019- ijgnd
+#           (c) 2020 jamesnicolas
 
 
 import datetime
@@ -10,16 +11,22 @@ from pprint import pprint as pp
 from anki.hooks import wrap
 import aqt
 from aqt import gui_hooks
+
+from aqt.deckbrowser import DeckBrowser
 from aqt.overview import Overview
+from aqt.theme import theme_manager
 
 
 def gc(arg, fail=False):
-    return aqt.mw.addonManager.getConfig(__name__).get(arg, fail)
+    conf = aqt.mw.addonManager.getConfig(__name__)
+    if conf:
+        return conf.get(arg, fail)
+    else:
+        return fail
 
 
-def whenIsNextLrnDue():
-    did = str(aqt.mw.col.decks.current()["id"])
-    o = aqt.mw.col.db.all("""select id, due from cards where queue = 1 and did = """ + did + """ order by due""")
+def whenIsNextLrnDue(sqlstring):
+    o = aqt.mw.col.db.all(sqlstring)
     if o:
         o = dict(o)
         now = datetime.datetime.now()
@@ -47,18 +54,19 @@ def whenIsNextLrnDue():
                 if v == c:
                     cid = k
                     break
-            # pp('###########' + str(cid) + '    ' + str(c))
             cdo = datetime.datetime.fromtimestamp(c)
             if gc("time_with_seconds", True):
                 f = "%H:%M:%S"
             else:
                 f = "%H:%M"
-            tstr = '''<a href=# style="text-decoration: none; color:black;"
-            onclick="return pycmd('BrowserSearch#%s')">%s (%s)</a>''' % (str(cid), cdo.strftime(f), timeInAgo(cdo))
+            color = "white" if theme_manager.night_mode else "black"
+            tstr = f'''<a href=# style="text-decoration: none; color:{color};"
+            onclick="return pycmd('BrowserSearch#{str(cid)}')">{cdo.strftime(f)} ({timeInAgo(cdo)})</a>'''
             msg = gc("sentence_beginning", "The next learning card due today is due at ") + tstr
             return "<div>" + msg + "</div>"
     else:
         return ""
+
 
 def timeInAgo(t):
     zero = datetime.timedelta(0)
@@ -85,10 +93,25 @@ def timeInAgo(t):
         msg += " ago"
     return msg
 
+
+def deckbrowserMessage(self, _old):
+    sql_string_all = """select id, due from cards where queue = 1 order by due"""
+    out = whenIsNextLrnDue(sql_string_all)
+    if out:
+        return _old(self) + out
+    else:
+        return _old(self)
+# TODO newstyle hooks -  maybe gui_hooks.deck_browser_will_render_content
+DeckBrowser._renderStats = wrap(DeckBrowser._renderStats, deckbrowserMessage, "around")
+
+
 def addRemainingTimeToDesc(overview, content):
-    content.desc += whenIsNextLrnDue()
+    did = str(aqt.mw.col.decks.current()["id"])
+    sql_string_deck = """select id, due from cards where queue = 1 and did = """ + did + """ order by due"""
+    content.desc += whenIsNextLrnDue(sql_string_deck)
 
 gui_hooks.overview_will_render_content.append(addRemainingTimeToDesc)
+
 
 def openBrowser(searchterm):
     browser = aqt.dialogs.open("Browser", aqt.mw)
@@ -107,3 +130,4 @@ def myLinkHandler(self, url, _old):
     else:
         return _old(self, url)
 Overview._linkHandler = wrap(Overview._linkHandler, myLinkHandler, "around")
+DeckBrowser._linkHandler = wrap(DeckBrowser._linkHandler, myLinkHandler, "around")
